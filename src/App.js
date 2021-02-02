6d11bf0856dfb191eb4c5ab0df61e916e2372a9e
jessand77 <46941395+jessand77@users.noreply.github.com>
2021-02-02 09:39:58 +0100

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import HelloUser from './HelloUser';
+import folkesson from './folkesson.png';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Abrakadabra vi får se hur det fungerar!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <br />
+        <img src={folkesson} alt="folkesson" />
         <HelloUser />
       </header>
     </div>
@@ -26,3 +15,4 @@ function App() {
 }
 
 export default App;
+
